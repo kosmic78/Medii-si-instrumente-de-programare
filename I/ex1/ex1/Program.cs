dab368d3d527995a49b3d8a87470550aab01e1d4
Cristian Gabor <92177475+kosmic78@users.noreply.github.com>
2024-02-11 21:43:37 +0200

    Add files via upload

---
+++ I/ex1/ex1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class App
+{
+    static void Main()
+    {
+        string input1 = Console.ReadLine();
+        string input2 = Console.ReadLine();
+
+        if (int.TryParse(input1, out int num1) && int.TryParse(input2, out int num2))
+            Console.WriteLine(num1 + num2);
+        else
+            Console.WriteLine("Wrong input!");
+    }
+}
---
+++ I/ex2/ex2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+class App
+{
+    static void Main()
+    {
+        List<int> numbers = new List<int>();
+
+        while (true)
+        {
+            Console.Write("Enter a number: ");
+            string input = Console.ReadLine();
+
+            if (input.EndsWith("00"))
+            {
+                break;
+            }
+
+            if (int.TryParse(input, out int num))
+            {
+                numbers.Add(num);
+            }
+            else
+            {
+                Console.WriteLine("Wrong input!");
+            }
+        }
+
+        Console.WriteLine("Numbers entered:");
+
+        foreach (var number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
